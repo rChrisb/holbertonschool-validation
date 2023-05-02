3b4964a493facc35b5876ee2a775ba716ef240fc
rChrisb <5767@holbertonstudents.com>
2023-05-02 15:14:16 +0200

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -3,7 +3,6 @@
 apt-get update && apt-get install -y hugo make
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-tar -xf hugo_extended_0.84.0_Linux-64bit.tar.gz
-mv hugo /usr/local/bin/hugo
+sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 
 make build
