e0de642e7904761ee0efe3843eda6bc13d0725a1
rChrisb <5767@holbertonstudents.com>
2023-05-02 17:17:27 +0200

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -8,6 +8,6 @@ apt-get update && apt-get install -y hugo make
 git clone https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
 sed -i 's/^#theme.*/theme = "ananke"/' config.toml
 make build
-# mv public/ dist/
+mv public/ dist/
 
 exit 0
