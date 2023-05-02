d6e54ef14766220e1b003e1bae8277c083139422
rChrisb <113911066+rChrisb@users.noreply.github.com>
2023-05-02 18:39:36 +0200

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -2,7 +2,7 @@ docker run -it ubuntu:18.04
 hugo version
 apt-get update && apt-get install -y hugo make
 
-make build
+# make build
 echo "nom d'un raton laveur qui ne lave pas bien" >> dist/index.html
 
 # exit 0
