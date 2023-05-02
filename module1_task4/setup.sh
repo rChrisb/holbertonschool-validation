5a44d4e8c8446be103b4aaab1f8fa0f60a7fb07b
rChrisb <5767@holbertonstudents.com>
2023-05-02 15:48:56 +0200

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,23 +1,16 @@
 
-
-docker run -it ubuntu:18.04
-
-
-if ! command -v hugo &> /dev/null; then
-    apt-get update && apt-get install -y hugo
-    if ! [ $? -eq 0 ];
-        exit 1
-    fi
+if ! command -v hugo >/dev/null 2>&1 || ! command -v make >/dev/null 2>&1; then
+  apt-get update && apt-get install -y hugo make || exit 1
 fi
 
-if ! command -v make &> /dev/null; then
-    apt-get update && apt-get install -y make
-    if ! [ $? -eq 0 ]; then
-        exit 1
-    fi
+if [[ ! -d ./dist ]]; then
+  mkdir ./dist || exit 1
 fi
 
-make build
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+cd ./site || exit 1
+
+make build || exit 1
+
+mv ./public/* ../dist/ || exit 1
+
+exit 0
