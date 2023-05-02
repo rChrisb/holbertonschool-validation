de7ede49d31e3a0667bf652b3808f1f0c6720037
rChrisb <5767@holbertonstudents.com>
2023-05-02 15:39:12 +0200

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,8 +1,7 @@
+set -e
+
 docker run -it ubuntu:18.04
 
-apt-get update && apt-get install -y hugo make wget
-
-wget https://github.com/gohugoio/hugo/releases/download/v0.111.0/hugo_extended_0.111.0_Linux-64bit.tar.gz
-dpkg -i hugo_extended_0.111.0_Linux-64bit.deb
+apt-get update && apt-get install -y hugo make
 
 make build
