9663c46fd53f42525a2be153c5a4b03fad3ccace
rChrisb <5767@holbertonstudents.com>
2023-05-02 17:41:45 +0200

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-docker run -it ubuntu:18.04
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 
 hugo version
 apt-get update && apt-get install -y hugo make
 
-git clone https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+# git clone https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
 # echo "theme = 'ananke'" >> config.toml
 # echo "publishDir = './dist'" >> config.toml
 make build
-rm -rf dist
-mkdir -p dist
-cp -R public/* dist/
+
 # mv public/ dist/
 
 exit 0
