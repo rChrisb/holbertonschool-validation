bab813a70d83ad69df31feb8eb93a416dd9865ce
rChrisb <5767@holbertonstudents.com>
2023-05-15 15:09:12 +0200

    Building Inside a Docker Container

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -5,6 +5,6 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.
 tar -xf hugo_extended_0.84.0_Linux-64bit.tar.gz
 mv hugo /usr/local/bin/
 rm -rf hugo_extended_0.84.0_Linux-64bit.tar.gz
-make build
+
 
 
