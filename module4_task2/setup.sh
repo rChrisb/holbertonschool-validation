b94b207c446e1e916de2d8fb45800208a6ab0605
rChrisb <5767@holbertonstudents.com>
2023-05-18 17:14:20 +0200

    Testing Docker Images

---
+++ module4_task2/setup.sh
@@ -0,0 +1,9 @@
+docker run -it ubuntu:18.04
+
+apt-get update && apt-get install -y wget make
+# wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+# tar -xf hugo_extended_0.84.0_Linux-64bit.tar.gz
+# mv hugo /usr/local/bin/
+# rm -rf hugo_extended_0.84.0_Linux-64bit.tar.gz
+
+
